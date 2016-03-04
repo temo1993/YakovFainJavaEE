1e18a953e63d0c51a9dbed4e91bdc9f4df44ca5f
Temo Kiknavelidze <temo.kiknavelidze@gmail.com>
2016-03-05 01:14:56 +0400

    ...

--- src/FindBooksServlet.java
+++ src/FindBooksServlet.java
@@ -13,30 +13,18 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/books")
 public class FindBooksServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public FindBooksServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		PrintWriter out = response.getWriter();
         out.println("Hello from FindBooks");
         
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 	}
 
 }
